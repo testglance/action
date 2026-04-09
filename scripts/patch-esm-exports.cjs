501a572fe379d4756f6dab64bb3780b18962d38d
Max Novak <maxim.novak@gmail.com>
2026-04-09 23:21:27 +0100

    style: format patch script with prettier

--- scripts/patch-esm-exports.cjs
+++ scripts/patch-esm-exports.cjs
@@ -2,9 +2,10 @@ const fs = require('fs');
 const path = require('path');
 
 const nodeModules = path.join(__dirname, '..', 'node_modules');
-const packages = fs.readdirSync(path.join(nodeModules, '@actions'))
-  .map(name => path.join(nodeModules, '@actions', name, 'package.json'))
-  .filter(p => fs.existsSync(p));
+const packages = fs
+  .readdirSync(path.join(nodeModules, '@actions'))
+  .map((name) => path.join(nodeModules, '@actions', name, 'package.json'))
+  .filter((p) => fs.existsSync(p));
 
 for (const pkgPath of packages) {
   const pkg = JSON.parse(fs.readFileSync(pkgPath, 'utf8'));
